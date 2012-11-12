985ad6eae813f086307654e74947cee440a92f44
zaziemo <zaziemo@gmail.com>
2012-11-12 20:59:11 +0100

    new tests: user gets id if her text is saved

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   test "a new user is not an admin" do
     user = User.new
     assert user.is_admin? == false, "New user should not be an admin"
@@ -8,7 +9,19 @@ class UserTest < ActiveSupport::TestCase
 
   test "an admin user is recognised as an admin" do
     user = User.new(:name => "tyranja")
-
-    assert user.is_admin? == true, "Tyranja should be our admin!"
+	assert user.is_admin? == true, "Tyranja should be our admin!"
   end
+
+  test "a new user has no user_id" do
+  	user = User.new
+  	assert user.id == nil, "a new user should not have an id"
+  end
+
+  test "new user gets id when her text is saved" do
+  	user = User.new
+  	user.save
+  	assert user.id != nil, "a new user gets and id when her text is saved"
+  end
+
 end
+
