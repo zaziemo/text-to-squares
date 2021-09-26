d98cbc47ee62bb3388be6110e727c7be47f54aa4
zaziemo <maren.heltsche@posteo.de>
2021-09-26 10:54:13 +0200

    add list view and template

--- text/views.py
+++ text/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Content
 
-# Create your views here.
+def text_list(request):
+    contents = Content.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'text/text_list.html', {'contents': contents})
